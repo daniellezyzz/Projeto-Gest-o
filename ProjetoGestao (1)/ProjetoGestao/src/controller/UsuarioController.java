78c23ca3347c4709528ff96c7936bdaba7e5fbf8
Lucas Azevedo <lucasazevedodesouza86@gmail.com>
2025-09-24 02:46:48 -0300

    Implementa Validador, UsuarioConcreto e atualiza UsuarioController

--- ProjetoGestao (1)/ProjetoGestao/src/controller/UsuarioController.java
+++ ProjetoGestao (1)/ProjetoGestao/src/controller/UsuarioController.java
@@ -2,18 +2,25 @@ package controller;
 
 import model.Perfil;
 import model.Usuario;
+import app.models.Validador;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Controller para gerenciar usuários em memória.
- */
 public class UsuarioController {
     private final List<Usuario> usuarios = new ArrayList<>();
 
     public void adicionarUsuario(Usuario u) {
+        if (!Validador.validarEmail(u.getEmail())) {
+            System.out.println("Email inválido: " + u.getEmail());
+            return;
+        }
+        if (!Validador.validarCPF(u.getCpf())) {
+            System.out.println("CPF inválido: " + u.getCpf());
+            return;
+        }
         usuarios.add(u);
+        System.out.println("Usuário cadastrado com sucesso: " + u.getNome());
     }
 
     public List<Usuario> listarUsuarios() {
@@ -35,3 +42,4 @@ public class UsuarioController {
         return r;
     }
 }
+
